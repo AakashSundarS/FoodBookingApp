d35af07713dba8ecc31eac65962f21372a2c55f1
Aakash <saakashsundar007@gmail.com>
2020-07-22 15:37:57 +0530

    modified:   App.js 	modified:   app.json 	new file:   assets/fonts/ionicons.eot 	new file:   assets/fonts/ionicons.scss 	new file:   assets/fonts/ionicons.svg 	new file:   assets/fonts/ionicons.ttf 	new file:   assets/fonts/ionicons.woff 	new file:   assets/fonts/ionicons.woff2 	new file:   assets/fonts/noto-sans-bold.ttf 	new file:   assets/fonts/noto-sans-bold.woff 	new file:   assets/fonts/noto-sans-regular.ttf 	new file:   assets/fonts/noto-sans-regular.woff 	new file:   assets/fonts/noto-sans.scss         new file:   assets/fonts/roboto-bold.ttf 	new file:   assets/fonts/roboto-bold.woff 	new file:   assets/fonts/roboto-bold.woff2 	new file:   assets/fonts/roboto-light.ttf 	new file:   assets/fonts/roboto-light.woff

--- App.js
+++ App.js
@@ -1,21 +1,134 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import logo from "./assets/foodimgs/chickennoodles.jpg"
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const onPress1 = () => setCount(prevCount => prevCount + 1);
+  const onPress2 = () => setCount(prevCount => prevCount - 1);
+  const onPress3 = () => alert("Please wait while your order is being processed...");
+  
+  if(count === 0){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+        <Text style={{alignItems: "center", color:'yellow', fontSize:40, padding:30}}>Chicken Noodles</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress1}
+      >
+        <Text style={{color:'green'}}>+ 1</Text>
+      </TouchableOpacity>
+
+      <View style={styles.countContainer}>
+        <Text>Total Items: {count}</Text>
+      </View>
+
+      <View style={styles.countContainer}>
+        <Text>Total: {count*40}</Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.checkout}
+        onPress={onPress3}
+      >
+        <Text>Confirm Order!</Text>
+      </TouchableOpacity>
+      
     </View>
-  );
-}
+  );}
+  else if (count === 10){
+      return (
+    <View style={styles.container}>
+
+        <Text style={{alignItems: "center", color:'yellow', fontSize:40, padding:30}}>Chicken Noodles</Text>
+      
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress2}
+      >
+        <Text style={{color:'red'}}>- 1</Text>
+      </TouchableOpacity>
+
+      <View style={styles.countContainer}>
+        <Text>Total Items: {count}</Text>
+      </View>
+
+      <View style={styles.countContainer}>
+        <Text>Total: {count*40}</Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.checkout}
+        onPress={onPress3}
+      >
+        <Text>Confirm Order!</Text>
+      </TouchableOpacity>
+      
+    </View>
+  );}
+  else{
+    return (
+    <View style={styles.container}>
+
+        <Text style={{alignItems: "center", color:'yellow', fontSize:40, padding:30}}>Chicken Noodles</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress1}
+      >
+        <Text style={{color:'green'}}>+ 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress2}
+      >
+        <Text style={{color:'red'}}>- 1</Text>
+      </TouchableOpacity>
+
+      <View style={styles.countContainer}>
+        <Text>Total Items: {count}</Text>
+      </View>
+
+      <View style={styles.countContainer}>
+        <Text>Total: {count*40}</Text>
+      </View>
+
+      <TouchableOpacity
+        style={styles.checkout}
+        onPress={onPress3}
+      >
+        <Text>Confirm Order!</Text>
+      </TouchableOpacity>
+      
+    </View>
+  );}}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 50,
+    backgroundColor: "rgba(0,0,0,0.9)"
   },
+  button: {
+    alignItems: "center",
+    backgroundColor: "rgba(0,255,255,1)",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "yellow"
+  },
+  checkout: {
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "rgba(255,0,0,0.8)"
+  }
 });
+
+export default App;
